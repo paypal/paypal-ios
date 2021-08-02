fe0e7315b445464a183f656752142f23a081e7ef
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-08-02 11:32:51 -0500

    Add PayPal.xcworkspace with PayPal.xcodeproj for package source code & Demo.xcodeproject for demo app
    Signed-off-by: Sarah Koop <skoop@paypal.com>

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Cannillo, Sammy on 8/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
