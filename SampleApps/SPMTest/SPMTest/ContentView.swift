4ebcb90139521e02b2cc40d2a7eb012dd76091c9
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-08-06 10:23:34 -0500

    Add build command for SPMTest app to build.yml on GH Actions

---
+++ SampleApps/SPMTest/SPMTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SPMTest
+//
+//  Created by Cannillo, Sammy on 8/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleApps/SPMTest/SPMTest/SPMTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMTestApp.swift
+//  SPMTest
+//
+//  Created by Cannillo, Sammy on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SPMTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
