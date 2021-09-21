78927dbd0666139901ec77875be3b747e028af7f
Jon Jones <jonathajones@paypal.com>
2021-09-21 10:35:14 -0500

    Card client updates + expiry tests

--- Sources/PaymentsCore/Networking/Models/Requests/ConfirmPaymentSourceRequest.swift
+++ Sources/PaymentsCore/Networking/Models/Requests/ConfirmPaymentSourceRequest.swift
@@ -13,8 +13,6 @@ public struct ConfirmPaymentSourceRequest: APIRequest {
     public var body: Data?
 
     public var headers: [HTTPHeader: String] {
-//        let encodedClientID = "\(clientID):".data(using: .utf8)?.base64EncodedString() ?? ""
-
         // TODO: Remove when this endpoint is updated and can be used with low-scoped token
         // For now, include your clientSecret here and your clientID in CoreConfig to test this request
         let clientSecret = ""
@@ -27,11 +25,19 @@ public struct ConfirmPaymentSourceRequest: APIRequest {
         ]
     }
 
+    /// Creates a request to attach a payment source to a specific order.
+    /// In order to use this initializer, the `paymentSource` parameter has to
+    /// contain the entire dictionary as it exists underneath the `payment_source` key.
+    ///
+    /// For more information on the expected body structure, see PayPal's internal
+    /// [API reference](https://ppaas/api/3719176155270329#apiReference)
     public init<T: Encodable>(
         paymentSource: T,
         orderID: String,
         clientID: String
     ) throws {
+        let paymentSource = ["payment_source": paymentSource]
+
         self.clientID = clientID
         self.orderID = orderID
 
--- UnitTests/CardTests/Card_Tests.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import Card
-
-final class PayPalTests: XCTestCase {
-    func testExample() {}
-}
