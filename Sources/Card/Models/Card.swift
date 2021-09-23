26a3b53fd5cae87995f54ceab38b518d1df17179
Minh Nguyen <thenguyen@paypal.com>
2021-09-23 14:02:47 -0400

    change Card.securityCode from optional -> required

--- Sources/Card/Models/Card.swift
+++ Sources/Card/Models/Card.swift
@@ -10,14 +10,14 @@ public struct Card: Codable {
 
     public var number: String
     public var expiry: CardExpiry
-    public var securityCode: String?
+    public var securityCode: String
 
     /// Initialize a card object
     /// - Parameters:
     ///   - number: The card number
     ///   - expiry: The card's expiration date
     ///   - securityCode: The card's security code (CVV, CVC, CVN, CVE, or CID)
-    public init(number: String, expiry: CardExpiry, securityCode: String? = nil) {
+    public init(number: String, expiry: CardExpiry, securityCode: String) {
         self.number = number
         self.expiry = expiry
         self.securityCode = securityCode
