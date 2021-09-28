cfa995bce38718ed0e39e4fba9a78902f65238b7
Minh Nguyen <thenguyen@paypal.com>
2021-09-28 11:31:54 -0400

    address comments: - remove PaymentsCore_Tests - add inline documentation to URLProtocolMock

--- Tests/NetworkingMocks/URLProtocolMock.swift
+++ Tests/NetworkingMocks/URLProtocolMock.swift
@@ -9,22 +9,26 @@ class URLProtocolMock: URLProtocol {
 
     // MARK: - Variables
 
+    /// The list of mock request/response that URLProtocol will process
     static var requestResponses: [ResponseMock] = []
 
     // MARK: - Override URLProtocol Functions
 
     override class func canInit(with request: URLRequest) -> Bool {
+        // Determine if URLProtocolMock should process this request
         true
     }
 
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        // Returns a canonical version of the request if needed, otherwise returns the request
         request
     }
 
     override func startLoading() {
+        // Start processing a request
         guard let client = client else { return }
 
-        // Here we try to match the request to the proper RequestResponseMock otherwise don't
+        // Here we try to match the request to the proper ResponseMock otherwise don't
         guard
             let firstIndex = Self.requestResponses
                 .firstIndex(where: { $0.canHandle(request: request) })
@@ -36,12 +40,14 @@ class URLProtocolMock: URLProtocol {
             client.urlProtocolDidFinishLoading(self)
             return
         }
+
+        // If there is a mock response for the request, pop it from requestResponses and vend mock response
         let requestResponse = Self.requestResponses.remove(at: firstIndex)
         resolveRequest(with: requestResponse, client: client)
     }
 
     override func stopLoading() {
-        // no-op
+        // URLProtocolMock has finished processing the request. No-op.
     }
 
     // MARK: - Private Functions
