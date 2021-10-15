19ebdeb7523568ca50ce6e02ca20cdcce425ccf0
Steven <sshropshire@paypal.com>
2021-10-15 15:36:50 -0500

    Fix coverage build error.

--- UnitTests/TestShared/HttpClient/MockHTTPClient.swift
+++ UnitTests/TestShared/HttpClient/MockHTTPClient.swift
@@ -6,32 +6,22 @@ class MockHTTPClient: HTTP {
     
     // MARK: - Properties
     private var defaultSendStub: HTTPResponse?
-    
-    private var sendStubs: [URLRequest: HTTPResponse] = [:]
-    private var pendingCallbacks: [URLRequest: ((HTTPResponse) -> Void)] = [:]
+    private var pendingCallback: ((HTTPResponse) -> Void)?
     
     private(set) var lastSentRequest: URLRequest?
     
     // MARK: - HTTP
     func send(_ urlRequest: URLRequest, completion: @escaping (HTTPResponse) -> Void) {
         lastSentRequest = urlRequest
-        if let stub = sendStubs[urlRequest] ?? defaultSendStub {
+        if let stub = defaultSendStub {
             completion(stub)
         } else {
-            pendingCallbacks[urlRequest] = completion
+            pendingCallback = completion
         }
     }
     
     // MARK: - MockHTTPClient
     func stubSend(with httpResponse: HTTPResponse) {
         defaultSendStub = httpResponse
-    }
-    
-    func stubSend(_ urlRequest: URLRequest, with httpResponse: HTTPResponse) {
-        if let completion = pendingCallbacks[urlRequest] {
-            completion(httpResponse)
-        } else {
-            sendStubs[urlRequest] = httpResponse
-        }
     }
 }
