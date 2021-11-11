88cfc73cb9158307ed65703984ae52ae419734d9
Jax DesMarais-Leder <jdesmarais@paypal.com>
2021-11-11 09:20:01 -0600

    bump PayPalCheckout from 0.73.0 to 0.77.0

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "PayPalCheckout", url: "https://github.com/paypal/paypalcheckout-ios", .exact("0.73.0"))
+        .package(name: "PayPalCheckout", url: "https://github.com/paypal/paypalcheckout-ios", .exact("0.77.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
