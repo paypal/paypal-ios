f772c94e1ce9db4a8adb65c6e4a6772dcdcc0e08
Jax DesMarais-Leder <jax.desmarais@gmail.com>
2022-02-03 14:17:13 -0600

    PayPal and PayPal Credit Buttons SwiftUI Representable (#34)
    * Add Coordinator class to pass action to UIKit button in SwiftUI
    * Add PayPalButton and PayPalCreditButton as UIViewRepresentable conforming final classes
    * Add both buttons to the SwiftUIPayPalDemo
    * Moved shared PayPal logic to BaseViewModel - this could probably use a refactor in the future
    * Update docs

--- Demo/Demo/FeatureViewControllers/PayPalDemoViewController.swift
+++ Demo/Demo/FeatureViewControllers/PayPalDemoViewController.swift
@@ -40,12 +40,12 @@ class PayPalDemoViewController: FeatureBaseViewController {
         NSLayoutConstraint.activate([
             payPalButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.layoutSpacing),
             payPalButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.layoutSpacing),
-            payPalButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: buttonSpacing),
+            payPalButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -buttonSpacing),
             payPalButton.heightAnchor.constraint(equalToConstant: Constants.textFieldHeight),
 
             payPalCreditButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.layoutSpacing),
             payPalCreditButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.layoutSpacing),
-            payPalCreditButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -buttonSpacing),
+            payPalCreditButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: buttonSpacing),
             payPalCreditButton.heightAnchor.constraint(equalToConstant: Constants.textFieldHeight)
         ])
     }
@@ -66,32 +66,6 @@ class PayPalDemoViewController: FeatureBaseViewController {
     // MARK: - PayPal Module Integration
 
     @objc func paymentButtonTapped() {
-        guard let orderID = baseViewModel.orderID else {
-            baseViewModel.updateTitle("Failed: missing orderID.")
-            return
-        }
-
-        checkoutWithPayPal(orderID: orderID)
-    }
-
-    func checkoutWithPayPal(orderID: String) {
-        let config = CoreConfig(clientID: DemoSettings.clientID, environment: DemoSettings.environment.paypalSDKEnvironment)
-        let payPalClient = PayPalClient(config: config, returnURL: DemoSettings.paypalReturnUrl)
-        let payPalRequest = PayPalRequest(orderID: orderID)
-
-        payPalClient.start(request: payPalRequest, presentingViewController: self) { [weak self] state in
-            guard let self = self else { return }
-            switch state {
-            case .success(let result):
-                self.baseViewModel.updateTitle("\(DemoSettings.intent.rawValue.capitalized) status: APPROVED")
-                print("✅ Order is successfully approved and ready to be captured/authorized with result: \(result)")
-            case .failure(let error):
-                self.baseViewModel.updateTitle("\(DemoSettings.intent) failed: \(error.localizedDescription)")
-                print("❌ There was an error: \(error)")
-            case .cancellation:
-                self.baseViewModel.updateTitle("\(DemoSettings.intent) canceled")
-                print("❌ Buyer has canceled the PayPal flow")
-            }
-        }
+        baseViewModel.payPalButtonTapped(presentingViewController: self)
     }
 }
