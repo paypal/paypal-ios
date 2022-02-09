843f7f2c14260c52a1d6db2f73c8da7681affdf6
sshropshire <sshropshire@paypal.com>
2022-02-09 15:45:23 -0600

    Add throws to swift doc.

--- Sources/Card/CardClient.swift
+++ Sources/Card/CardClient.swift
@@ -24,7 +24,8 @@ public class CardClient {
     /// Approve an order with a card, which validates buyer's card, and if valid, attaches the card as the payment source to the order.
     /// After the order has been successfully approved, you will need to handle capturing/authorizing the order in your server.
     /// - Parameter request: The request containing the card and order id for approval
-    /// - Returns: Card result or throws an error
+    /// - Returns: Card result
+    /// - Throws: PayPalSDK error if approve order could not complete successfully
     public func approveOrder(request: CardRequest) async throws -> CardResult {
         let confirmPaymentRequest = try ConfirmPaymentSourceRequest(
             card: request.card,
