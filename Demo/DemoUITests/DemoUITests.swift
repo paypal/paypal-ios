1224223faf54bf30159f8ee888f8c2d1e9ce429e
Jose Noriega <jose.noriega.defferrari@gmail.com>
2022-02-22 11:59:45 -0600

    Update demo app config to only test potrait mode.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- Demo/DemoUITests/DemoUITests.swift
+++ Demo/DemoUITests/DemoUITests.swift
@@ -7,8 +7,31 @@ class DemoUITests: XCTestCase {
         continueAfterFailure = false
     }
 
+    private func typeNumbers(app: XCUIApplication, cardNumber: String) {
+        let keys = Array(cardNumber).map(String.init)
+        for key in keys {
+            app.keys[key].tap()
+        }
+        print(keys)
+    }
+
     func testExample() throws {
-        _ = launchApp()
+        let app = launchApp()
+
+        app.staticTexts["Create Order"].tap()
+        _ = app.staticTexts["Order ID:"].waitForExistence(timeout: 10.0)
+
+        app.textFields["Card Number"].tap()
+        typeNumbers(app: app, cardNumber: "4111111111111111")
+
+        app.textFields["Expiration"].tap()
+        typeNumbers(app: app, cardNumber: "0220")
+
+        app.textFields["CVV"].tap()
+        typeNumbers(app: app, cardNumber: "123")
+
+        app.buttons["Capture Order"].tap()
+
         XCTAssertTrue(true)
     }
 }
