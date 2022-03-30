52297f9cd146311388b3cc0b02119692dbef9692
Jose Noriega <jonoriega@paypal.com>
2022-03-30 13:29:17 -0300

    Remove core dependecy on PayPalDataCollector

---
+++ Sources/PayPalDataCollector/PayPalDataCollectorEnvironment.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+public enum PayPalDataCollectorEnvironment {
+    case sandbox
+    case production
+}
--- UnitTests/PayPalDataCollector/PayPalDataCollector_Tests.swift
+++ UnitTests/PayPalDataCollector/PayPalDataCollector_Tests.swift
@@ -1,24 +1,20 @@
 import XCTest
-import PaymentsCore
 @testable import PayPalDataCollector
 
 class PayPalDataCollector_Tests: XCTestCase {
-
-    private let sandboxConfig = CoreConfig(clientID: "", environment: .sandbox)
-    private let productionConfig = CoreConfig(clientID: "", environment: .production)
 
     private var deviceInspector = MockDeviceInspector()
     private var magnesSDK = MockMagnesSDK()
 
     func testCollectDeviceData_setsMagnesEnvironmentToSANDBOX() {
-        let sut = PayPalDataCollector(config: sandboxConfig, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
+        let sut = PayPalDataCollector(environment: .sandbox, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
         _ = sut.collectDeviceData()
 
         XCTAssertEqual(.SANDBOX, magnesSDK.capturedSetupParams?.env)
     }
 
     func testCollectDeviceData_setsMagnesEnvironmentToLIVE() {
-        let sut = PayPalDataCollector(config: productionConfig, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
+        let sut = PayPalDataCollector(environment: .production, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
         _ = sut.collectDeviceData()
 
         XCTAssertEqual(.LIVE, magnesSDK.capturedSetupParams?.env)
@@ -27,28 +23,28 @@ class PayPalDataCollector_Tests: XCTestCase {
     func testCollectDeviceData_setsMagnesAppGUIDToTheCurrentDeviceID() {
         deviceInspector.stubPayPalDeviceIdentifierWithValue("sample_device_identifier")
 
-        let sut = PayPalDataCollector(config: sandboxConfig, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
+        let sut = PayPalDataCollector(environment: .sandbox, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
         _ = sut.collectDeviceData()
 
         XCTAssertEqual("sample_device_identifier", magnesSDK.capturedSetupParams?.appGuid)
     }
 
     func testCollectDeviceData_disablesMagnesRemoteConfiguration() {
-        let sut = PayPalDataCollector(config: sandboxConfig, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
+        let sut = PayPalDataCollector(environment: .sandbox, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
         _ = sut.collectDeviceData()
 
         XCTAssertEqual(false, magnesSDK.capturedSetupParams?.isRemoteConfigDisabled)
     }
 
     func testCollectDeviceData_disablesMagnesBeacon() {
-        let sut = PayPalDataCollector(config: sandboxConfig, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
+        let sut = PayPalDataCollector(environment: .sandbox, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
         _ = sut.collectDeviceData()
 
         XCTAssertEqual(false, magnesSDK.capturedSetupParams?.isBeaconDisabled)
     }
 
     func testCollectDeviceData_setsMagnesSourceToPAYPAL() {
-        let sut = PayPalDataCollector(config: sandboxConfig, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
+        let sut = PayPalDataCollector(environment: .sandbox, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
         _ = sut.collectDeviceData()
 
         XCTAssertEqual(.PAYPAL, magnesSDK.capturedSetupParams?.source)
@@ -59,7 +55,7 @@ class PayPalDataCollector_Tests: XCTestCase {
         let magnesResult = MockMagnesSDKResult(payPalClientMetaDataId: "new_client_metadata_id")
         magnesSDK.stubCollectDeviceData(forArgs: args, withValue: magnesResult)
 
-        let sut = PayPalDataCollector(config: sandboxConfig, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
+        let sut = PayPalDataCollector(environment: .sandbox, magnesSDK: magnesSDK, deviceInspector: deviceInspector)
         let result = sut.collectDeviceData(additionalData: ["sample": "data"])
 
         let expected = """
