1f0151585e71fb77ad17042d26fc189af7b7c4c2
jcnoriega <jose.noriega.defferrari@gmail.com>
2022-06-08 12:12:14 -0300

    Payment Button Configuration Demo in UKit (#53)
    * Added button customization demo
    
    * Update Demo/Demo/FeatureViewControllers/PaymentButtonCustomizationViewController.swift
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * Change spb variable name and removed comment
    
    * Remove Case Iterable, renamed variables
    
    * Added spaces, remove extra spaces, add inline functions
    
    * Removed extra spaces
    
    * Remove extra space
    
    * Payment Button Configuration SwiftUI (#54)
    
    * Add button demo in swiftui
    
    * Removed if else and spaces
    
    * Change comment and add credit in switch case
    
    Co-authored-by: Jose Noriega <jonoriega@paypal.com>
    
    Co-authored-by: Jose Noriega <jonoriega@paypal.com>
    Co-authored-by: Sarah Koop <skoop@paypal.com>

---
+++ Demo/Demo/Extensions/PaymentButtonEnums+Extension.swift
@@ -0,0 +1,96 @@
+import PayPalUI
+
+extension PayPalPayLaterButton.Color {
+
+    static func allCases() -> [PayPalPayLaterButton.Color] {
+        [
+            PayPalPayLaterButton.Color.gold,
+            PayPalPayLaterButton.Color.white,
+            PayPalPayLaterButton.Color.black,
+            PayPalPayLaterButton.Color.silver,
+            PayPalPayLaterButton.Color.blue
+        ]
+    }
+
+    static func allCasesAsString() -> [String] {
+        allCases().map { $0.rawValue }
+    }
+}
+
+extension PayPalButton.Color {
+
+    static func allCases() -> [PayPalButton.Color] {
+        [
+            PayPalButton.Color.gold,
+            PayPalButton.Color.white,
+            PayPalButton.Color.black,
+            PayPalButton.Color.silver,
+            PayPalButton.Color.blue
+        ]
+    }
+
+    static func allCasesAsString() -> [String] {
+        allCases().map { $0.rawValue }
+    }
+}
+
+extension PayPalCreditButton.Color {
+
+    static func allCases() -> [PayPalCreditButton.Color] {
+        [
+            PayPalCreditButton.Color.white,
+            PayPalCreditButton.Color.black,
+            PayPalCreditButton.Color.darkBlue
+        ]
+    }
+
+    static func allCasesAsString() -> [String] {
+        allCases().map { $0.rawValue }
+    }
+}
+
+extension PaymentButtonEdges {
+
+    static func allCases() -> [PaymentButtonEdges] {
+        [
+            PaymentButtonEdges.hardEdges,
+            PaymentButtonEdges.softEdges,
+            PaymentButtonEdges.rounded
+        ]
+    }
+
+    static func allCasesAsString() -> [String] {
+        allCases().map { $0.description }
+    }
+}
+
+extension PaymentButtonSize {
+
+    static func allCases() -> [PaymentButtonSize] {
+        [
+            PaymentButtonSize.mini,
+            PaymentButtonSize.collapsed,
+            PaymentButtonSize.expanded,
+            PaymentButtonSize.full
+        ]
+    }
+
+    static func allCasesAsString() -> [String] {
+        allCases().map { $0.description }
+    }
+}
+
+extension PaymentButtonFundingSource {
+
+    static func allCases() -> [PaymentButtonFundingSource] {
+        [
+            PaymentButtonFundingSource.payPal,
+            PaymentButtonFundingSource.payLater,
+            PaymentButtonFundingSource.credit
+        ]
+    }
+
+    static func allCasesAsString() -> [String] {
+        PaymentButtonFundingSource.allCases().map { $0.rawValue }
+    }
+}
