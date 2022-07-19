3d6b2cee97cfee6fe0371cb418e48d6936d73472
Karthik Gangineni <kgangineni@paypal.com>
2022-07-19 12:48:33 -0500

    Removed files moved to Demo

--- Sources/PaymentsCore/Networking/Models/Responses/AccessTokenResponse.swift
+++
@@ -1,9 +0,0 @@
-// swiftlint:disable space_after_main_type
-
-public struct AccessTokenResponse: Codable {
-    let scope: String
-    public let accessToken: String
-    let tokenType: String
-    let expiresIn: Int
-    let nonce: String
-}
