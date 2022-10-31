0205512188733504025f6e300c622b9a3654f34c
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2022-10-31 11:56:55 -0500

    WIP - start filling in AnalyticsEventParams metadta

--- Sources/PaymentsCore/AnalyticsEventParams.swift
+++ Sources/PaymentsCore/AnalyticsEventParams.swift
@@ -1,22 +1,71 @@
+import UIKit
+
+// swiftlint:disable identifier_name
+
 struct AnalyticsEventParams: Codable {
     
     // TODO: Convert to camel case, and then use snake-case encoding
-    let app_id: String
-    let app_name: String
-    let c_sdk_ver: String
-    let client_id: String
-    let client_os: String
-    let comp: String
-    let device_manufacturer: String
-    let event_name: String
-    let event_source: String
-    let ios_package_manager: String
-    let is_simulator: String
-    let mapv: String
-    let merchant_id: String
-    let mobile_device_model: String
-    let platform: String
-    let session_id: String
-    let t: String
-    let tenant_name: String
+    
+    var appId: String = Bundle.main.infoDictionary?[kCFBundleIdentifierKey as String] as? String ?? ""
+    
+    var appName: String = Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String ?? ""
+    
+//    let c_sdk_ver: String
+    
+//    let client_id: String
+    
+    var clientOs: String = UIDevice.current.systemName + " " + UIDevice.current.systemVersion
+    
+    var comp = "ppunifiedsdk"
+    
+    var deviceManufacturer = "Apple"
+    
+    var eventName: String
+    
+    var eventSource = "mobile-native"
+    
+    var iOSPackageManager: String {
+        #if COCOAPODS
+            return "CocoaPods"
+        #elseif SWIFT_PACKAGE
+            return "Swift Package Manager"
+        #else
+            return "Carthage or Other"
+        #endif
+    }
+    
+    var isSimulator: Bool {
+        #if targetEnvironment(simulator)
+        return true
+        #else
+        return false
+        #endif
+    }
+    
+    var mapv: String = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as? String ?? ""
+    
+//    let merchant_id: String
+    
+    var mobileDeviceModel: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
+    }
+    
+    var platform = "iOS"
+    
+//    let session_id: String
+    
+    var t = String(Date().timeIntervalSince1970 * 1000)
+    
+    var tenantName = "PayPal"
+    
+    init(eventName: String) {
+        self.eventName = eventName
+    }
 }
