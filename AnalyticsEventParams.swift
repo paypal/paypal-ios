95bd7cd2f916837a79be42cdf66672a0de1a6bd8
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2022-10-31 11:25:40 -0500

    Add AnalyticsEventParams codable to house analytics payload body

---
+++ AnalyticsEventParams.swift
@@ -0,0 +1,21 @@
+struct AnalyticsEventParams: Codable {
+    
+    let app_id: String
+    let app_name: String
+    let c_sdk_ver: String
+    let client_id: String
+    let client_os: String
+    let comp: String
+    let device_manufacturer: String
+    let event_name: String
+    let event_source: String
+    let ios_package_manager: String
+    let is_simulator: String
+    let mapv: String
+    let merchant_id: String
+    let mobile_device_model: String
+    let platform: String
+    let session_id: String
+    let t: String
+    let tenant_name: String
+}
