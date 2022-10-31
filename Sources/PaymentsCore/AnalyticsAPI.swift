b05449aeb385d289ef288f0c46e68e770023aa74
Matt Wylder <mwylder@paypal.com>
2022-10-31 11:20:28 -0500

    WIP - Add AnalyticsAPI and APIRequest protocol conformance
    Signed-off-by: Sammy Cannillo <scannillo@paypal.com>

---
+++ Sources/PaymentsCore/AnalyticsAPI.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyticsAPI.swift
+//  PaymentsCore
+//
+//  Created by Samantha Cannillo on 10/31/22.
+//
+
+import Foundation
---
+++ Sources/PaymentsCore/AnalyticsEventRequest.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyticsEventRequest.swift
+//  PaymentsCore
+//
+//  Created by Samantha Cannillo on 10/31/22.
+//
+
+import Foundation
