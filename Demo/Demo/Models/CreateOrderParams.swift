17a8bb1af45f2f3880a7a028339b593e3086fe31
Jose Noriega <jonoriega@paypal.com>
2022-11-03 14:41:17 -0300

    Add appContext to conifrmpayemntsource, remove from order creation

--- Demo/Demo/Models/CreateOrderParams.swift
+++ Demo/Demo/Models/CreateOrderParams.swift
@@ -2,7 +2,6 @@ struct CreateOrderParams: Codable {
 
     let intent: String
     var purchaseUnits: [PurchaseUnit]?
-    var applicationContext: ApplicationContext?
 }
 
 struct PurchaseUnit: Codable {
@@ -14,10 +13,4 @@ struct Amount: Codable {
 
     let currencyCode: String
     let value: String
-}
-
-struct ApplicationContext: Codable {
-
-    var returnUrl: String?
-    var cancelUrl: String?
 }
--- Sources/Card/ConfirmPaymentSourceRequest.swift
+++ Sources/Card/ConfirmPaymentSourceRequest.swift
@@ -5,12 +5,12 @@ import PaymentsCore
 
 /// Describes request to confirm a payment source (approve an order)
 struct ConfirmPaymentSourceRequest: APIRequest {
-
+    
     private let orderID: String
     private let pathFormat: String = "/v2/checkout/orders/%@/confirm-payment-source"
     private let accessToken: String
     private let jsonEncoder = JSONEncoder()
-
+    
     /// Creates a request to attach a payment source to a specific order.
     /// In order to use this initializer, the `paymentSource` parameter has to
     /// contain the entire dictionary as it exists underneath the `payment_source` key.
@@ -18,37 +18,62 @@ struct ConfirmPaymentSourceRequest: APIRequest {
         accessToken: String,
         cardRequest: CardRequest
     ) throws {
+        var confirmPaymentSourceBody = ConfirmPaymentSourceBody()
         var card = cardRequest.card
         if let threeDSecureRequest = cardRequest.threeDSecureRequest {
             let verification = Verification(method: threeDSecureRequest.sca.rawValue)
             card.attributes = Attributes(verification: verification)
+            
+            let applicationContext = ApplicationContext(
+                returnUrl: threeDSecureRequest.returnUrl,
+                cancelUrl: threeDSecureRequest.cancelUrl
+            )
+            confirmPaymentSourceBody.applicationContext = applicationContext
         }
-        let paymentSource = [ "payment_source": [ "card": card ] ]
-
+        
+        confirmPaymentSourceBody.paymentSource = PaymentSource(card: card)
+        
         self.orderID = cardRequest.orderID
         self.accessToken = accessToken
-
+        
         path = String(format: pathFormat, orderID)
-
+        
         jsonEncoder.keyEncodingStrategy = .convertToSnakeCase
-        body = try jsonEncoder.encode(paymentSource)
+        body = try jsonEncoder.encode(confirmPaymentSourceBody)
         
         // TODO - The complexity in this `init` signals to reconsider our use/design of the `APIRequest` protocol.
         // Existing pattern doesn't provide clear, testable interface for encoding JSON POST bodies.
     }
-
+    
     // MARK: - APIRequest
-
+    
     typealias ResponseType = ConfirmPaymentSourceResponse
-
+    
     var path: String
     var method: HTTPMethod = .post
     var body: Data?
-
+    
     var headers: [HTTPHeader: String] {
         [
             .contentType: "application/json", .acceptLanguage: "en_US",
             .authorization: "Bearer \(accessToken)"
         ]
+    }
+    
+    private struct ConfirmPaymentSourceBody: Encodable {
+        
+        var paymentSource: PaymentSource?
+        var applicationContext: ApplicationContext?
+    }
+    
+    private struct ApplicationContext: Encodable {
+        
+        let returnUrl: String
+        let cancelUrl: String
+    }
+    
+    private struct PaymentSource: Encodable {
+        
+        let card: Card
     }
 }
