afd699cb8354786fb3d55eb3a5d8500da3260700
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2022-11-07 13:35:41 -0600

    Fixup - silence linter by using caseless enum for files that only host static members

--- Sources/PaymentsCore/PaymentsCoreConstants.swift
+++ Sources/PaymentsCore/PaymentsCoreConstants.swift
@@ -1,5 +1,5 @@
 /// :nodoc: This class is exposed for internal PayPal use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
-public class PayPalCoreConstants {
+enum PayPalCoreConstants {
     
     // TODO: - Update release script to update this version #
     /// :nodoc: This property is exposed for internal PayPal use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
