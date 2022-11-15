cd1782d5f94edbf65ce840409f0c64381f8358be
Jose Noriega <jonoriega@paypal.com>
2022-11-15 14:18:18 -0300

    Docstring SCA

--- Sources/Card/Models/SCA.swift
+++ Sources/Card/Models/SCA.swift
@@ -1,6 +1,11 @@
 import UIKit
 
+/// Strong Customer Authentication launching options
 public enum SCA: String {
+    
+    /// Launch SCA for every transaction
     case scaAlways = "SCA_ALWAYS"
+    
+    /// Launch SCA only when applicable
     case scaWhenRequired = "SCA_WHEN_REQUIRED"
 }
