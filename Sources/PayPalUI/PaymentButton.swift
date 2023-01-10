217ecc8c5c512abb8bd7f4ede44f2de0308bcfd9
Jose Noriega <jonoriega@paypal.com>
2023-01-10 14:11:51 -0300

    Add if for SPM

--- Sources/PayPalUI/PaymentButton.swift
+++ Sources/PayPalUI/PaymentButton.swift
@@ -4,7 +4,12 @@ import UIKit
 public class PaymentButton: UIButton {
 
     // asset identifier path for image and color button assets
+    
+    #if SWIFT_PACKAGE
     static var bundle = Bundle.module
+    #else
+    static var bundle = Bundle(identifier: "com.paypal.ios-sdk.PayPalUI")
+    #endif
 
     // MARK: - Init
 
