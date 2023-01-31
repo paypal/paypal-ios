f4ccbd6869c9651fd8de45e0e6a860ce8c9a8bb2
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2023-01-30 19:40:10 -0600

    Revert accidental change in MockURLSession

--- UnitTests/TestShared/MockURLSession.swift
+++ UnitTests/TestShared/MockURLSession.swift
@@ -16,9 +16,4 @@ class MockURLSession: URLSessionProtocol {
             return (data, urlResponse)
         }
     }
-    
-    func performRequest(with urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
-        let cannedData = cannedJSONData?.data(using: String.Encoding.utf8)
-        completionHandler(cannedData, cannedURLResponse, cannedError)
-    }
 }
