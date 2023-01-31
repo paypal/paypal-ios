b4d388815d5e66726e9461b9253ee942cc0fb7bd
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2023-01-31 14:23:54 -0600

    Update docstring for APIClient.swift

--- Sources/PaymentsCore/Networking/APIClient.swift
+++ Sources/PaymentsCore/Networking/APIClient.swift
@@ -39,8 +39,11 @@ public class APIClient {
     public func fetch<T: APIRequest>(request: T) async throws -> (T.ResponseType) {
         return try await http.performRequest(request)
     }
-
+    
     /// :nodoc: This method is exposed for internal PayPal use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+    ///
+    /// Retrieves the merchant's clientID either from the local cache, or via an HTTP request if not cached.
+    /// - Returns: Merchant clientID.
     public func getClientID() async throws -> String {
         let request = GetClientIDRequest(accessToken: coreConfig.accessToken)
         let (response) = try await http.performRequest(request, withCaching: true)
