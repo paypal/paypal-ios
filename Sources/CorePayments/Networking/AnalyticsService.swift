3cb0ab23143f6369ba3faaa7a237faf9234a4769
Sammy Cannillo <scannillo@paypal.com>
2023-05-30 11:39:38 -0500

    WIP - move HTTPResponseParser & begin re-organizing tests

--- Sources/CorePayments/Networking/AnalyticsService.swift
+++ Sources/CorePayments/Networking/AnalyticsService.swift
@@ -66,27 +66,3 @@ public struct AnalyticsService {
         return response.clientID
     }
 }
-
-class HTTPResponseParser {
-    
-    private let decoder: JSONDecoder
-
-    init() {
-        decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-    }
-    
-    func parse<T: Decodable>(_ httpResponse: HTTPResponse, as type: T.Type) throws -> T {
-        guard let data = httpResponse.body else {
-            throw APIClientError.noResponseDataError
-        }
-        
-        if httpResponse.isSuccessful {
-            let decodedData = try decoder.decode(T.self, from: data)
-            return (decodedData)
-        } else {
-            let errorData = try decoder.decode(ErrorResponse.self, from: data)
-            throw APIClientError.serverResponseError(errorData.readableDescription)
-        }
-    }
-}
--- UnitTests/PaymentsCoreTests/APIClient_Tests.swift
+++ UnitTests/PaymentsCoreTests/APIClient_Tests.swift
@@ -26,6 +26,53 @@ class APIClient_Tests: XCTestCase {
         XCTAssertEqual(mockHTTP.lastAPIRequest?.path, "/fake-path")
     }
     
+    // todo - Should these be in a test file for the HTTPResponseParser instead?
+    func testFetch_whenBadStatusCode_withErrorData_returnsReadableErrorMessage() async {
+        let jsonResponse = """
+        {
+            "name": "ERROR_NAME",
+            "message": "The requested action could not be performed."
+        }
+        """
+        mockHTTP.stubHTTPResponse = HTTPResponse(status: 500, body: jsonResponse.data(using: .utf8)!)
+        
+        do {
+            _ =  try await sut.fetch(request: FakeRequest())
+            XCTFail("Request succeeded. Expected error.")
+        } catch let error as CoreSDKError {
+            XCTAssertEqual(error.domain, APIClientError.domain)
+            XCTAssertEqual(error.code, APIClientError.Code.serverResponseError.rawValue)
+            XCTAssertEqual(error.localizedDescription, "ERROR_NAME: The requested action could not be performed.")
+        } catch let error {
+            XCTFail("Unexpected error type")
+        }
+    }
+
+    func testFetch_whenBadStatusCode_withoutErrorData_returnsUnknownError() async {
+        mockHTTP.stubHTTPResponse = HTTPResponse(status: 500, body: nil)
+
+        do {
+            _ = try await sut.fetch(request: FakeRequest())
+            XCTFail("Request succeeded. Expected error.")
+        } catch let error as CoreSDKError {
+            XCTAssertEqual(error.domain, APIClientError.domain)
+            XCTAssertEqual(error.code, APIClientError.Code.unknown.rawValue)
+            XCTAssertEqual(error.localizedDescription, "An unknown error occured. Contact developer.paypal.com/support.")
+        } catch {
+            XCTFail("Unexpected error type")
+        }
+    }
+    
+    // test when status code bad & error body, parses
+    
+    // test when status code bad & no error body, returns X error
+    
+    // test when missing body data, returns no body data
+    
+    // test when body data & success code, parses body
+    
+    // test when body data bad & success code,
+    
     // MARK: - fetchCachedOrRemoteClientID()
 
     func testGetClientID_successfullyReturnsData() async throws {
