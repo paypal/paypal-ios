6a5a24bd9793c464a52a6c701e09d16265be0d57
Victoria Park <vicpark@paypal.com>
2023-05-31 10:40:04 -0700

    remove caching for clientID

--- Sources/CorePayments/Networking/APIClient.swift
+++ Sources/CorePayments/Networking/APIClient.swift
@@ -31,14 +31,4 @@ public class APIClient {
     public func fetch<T: APIRequest>(request: T) async throws -> (T.ResponseType) {
         return try await http.performRequest(request)
     }
-    
-    /// :nodoc: This method is exposed for internal PayPal use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
-    ///
-    /// Retrieves the merchant's clientID either from the local cache, or via an HTTP request if not cached.
-    /// - Returns: Merchant clientID.
-    public func fetchCachedOrRemoteClientID() async throws -> String {
-        let request = GetClientIDRequest(accessToken: coreConfig.clientID)
-        let (response) = try await http.performRequest(request, withCaching: true)
-        return response.clientID
-    }
 }
--- Sources/CorePayments/Networking/HTTP.swift
+++ Sources/CorePayments/Networking/HTTP.swift
@@ -5,27 +5,19 @@ class HTTP {
     
     let coreConfig: CoreConfig
     private var urlSession: URLSessionProtocol
-    private var urlCache: URLCacheable
     private let decoder = APIClientDecoder()
 
     init(
         urlSession: URLSessionProtocol = URLSession.shared,
-        urlCache: URLCacheable = URLCache(memoryCapacity: 4 * 1024 * 1024, diskCapacity: 0),
         coreConfig: CoreConfig
     ) {
         self.urlSession = urlSession
-        self.urlCache = urlCache
         self.coreConfig = coreConfig
     }
     
-    func performRequest<T: APIRequest>(_ request: T, withCaching cachingEnabled: Bool = false) async throws -> (T.ResponseType) {
+    func performRequest<T: APIRequest>(_ request: T) async throws -> (T.ResponseType) {
         guard let urlRequest = request.toURLRequest(environment: coreConfig.environment) else {
             throw APIClientError.invalidURLRequestError
-        }
-        
-        if cachingEnabled, let response = urlCache.cachedResponse(for: urlRequest) {
-            let decodedData = try decoder.decode(T.self, from: response.data)
-            return decodedData
         }
         
         let (data, response) = try await urlSession.performRequest(with: urlRequest)
@@ -36,10 +28,6 @@ class HTTP {
         switch response.statusCode {
         case 200..<300:
             let decodedData = try decoder.decode(T.self, from: data)
-            if cachingEnabled {
-                let cachedURLResponse = CachedURLResponse(response: response, data: data)
-                urlCache.storeCachedResponse(cachedURLResponse, for: urlRequest)
-            }
             return (decodedData)
         default:
             let errorData = try decoder.decode(from: data)
