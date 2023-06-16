eb8ccc19b6d47f49f3cc6bdf5f7f6e18a9871ce4
Victoria Park <vicpark@paypal.com>
2023-06-16 16:33:37 -0700

    add shouldVault and customerID docStrings

--- Sources/CardPayments/Models/CardRequest.swift
+++ Sources/CardPayments/Models/CardRequest.swift
@@ -11,8 +11,10 @@ public struct CardRequest {
     /// 3DS authentication launch option
     public let sca: SCA
     
+    /// The option to vault with purchase
     public let shouldVault: Bool
     
+    /// The customerID for vault with purchase option. If left blank, PayPal endpoint will auto-generate one
     public let customerID: String?
     
     /// Creates an instance of a card request
@@ -20,6 +22,8 @@ public struct CardRequest {
     ///    - orderID: The order to be approved
     ///    - card: The card to be charged for this order
     ///    - sca: Specificy to always launch 3DS or only when required. Defaults to `scaWhenRequired`.
+    ///    - shouldVault: The option to vault the card with purchase. Defaults to `false`.
+    ///    - customerID: The customer ID provided by the merchant. Defaults to `nil`.
     public init(orderID: String, card: Card, sca: SCA = .scaWhenRequired, shouldVault: Bool = false, customerID: String? = nil) {
         self.orderID = orderID
         self.card = card
