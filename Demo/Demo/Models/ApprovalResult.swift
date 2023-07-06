8e5b2c1f5d7192dd8585aa1ec8ece2bd00c0509c
Victoria Park <vicpark@paypal.com>
2023-07-05 21:25:43 -0700

    fix lint error

--- Demo/Demo/Models/ApprovalResult.swift
+++ Demo/Demo/Models/ApprovalResult.swift
@@ -2,9 +2,4 @@ struct ApprovalResult {
 
     let orderId: String
     let payerId: String
-
-    init(orderId: String, payerId: String) {
-        self.orderId = orderId
-        self.payerId = payerId
-    }
 }
