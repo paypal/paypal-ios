0703661b7c608c1bdb3581baace920d8e47e1351
Sammy Cannillo <scannillo@paypal.com>
2023-08-08 15:47:22 -0500

    Remove EligibilityAPI & GraphQL implementation

--- Sources/CorePayments/Networking/GraphQL/GraphQLQuery.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-protocol GraphQLQuery {
-    var query: String { get }
-    var variables: [String: Any] { get }
-    func requestBody() throws -> Data
-}
--- Sources/CorePayments/Networking/GraphQL/SupportedCountryCurrencyType.swift
+++
@@ -1,3 +0,0 @@
-enum SupportedCountryCurrencyType: String {
-    case USD
-}
