04938411ce0f51beab1c1cac761fb96ee1f8df0f
Sammy Cannillo <scannillo@paypal.com>
2023-08-10 10:12:43 -0500

    WIP - begin adding GraphQL template struct & method to APIClient

--- Sources/CorePayments/Networking/APIClient.swift
+++ Sources/CorePayments/Networking/APIClient.swift
@@ -42,7 +42,42 @@ public class APIClient {
     }
     
     // TODO: - Add GraphQL equivalent request type & function
-    // public func fetch(request: GraphQLRequest) async throws -> HTTPResponse { }
+//     public func fetch(request: GraphQLRequest) async throws -> HTTPResponse { }
+    public func fetch() throws {
+        let queryString = """
+            mutation UpdateVaultSetupToken(
+                $clientID: String!,
+                $vaultSetupToken: String!,
+                $paymentSource: PaymentSource
+            ) {
+                updateVaultSetupToken(
+                    clientId: $clientID
+                    vaultSetupToken: $vaultSetupToken
+                    paymentSource: $paymentSource
+                ) {
+                    id,
+                    status,
+                    links {
+                        rel, href
+                    }
+                }
+            }
+        """
+        
+        // TODO: - Move JSON encoding into custom class, similar to HTTPResponseParser
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        let variables = try encoder.encode(VaultDataEncodableVariables())
+        
+        // Combine varialbes & query into single post body
+        let graphQLRequest = GraphQLRequest(
+            queryNameForURL: "UpdateVaultSetupToken",
+            query: queryString,
+            variables: variables
+        )
+        
+        // Construct HTTPRequest
+    }
     
     // MARK: - Private Methods
     
@@ -61,3 +96,59 @@ public class APIClient {
         return url
     }
 }
+
+/// :nodoc:
+public struct GraphQLRequest {
+    let queryNameForURL: String?
+    let query: String
+    let variables: Data // Dictionary
+}
+
+struct VaultDataEncodableVariables: Encodable {
+    
+    // MARK: - Coding KEys
+    
+    private enum TopLevelKeys: String, CodingKey {
+        case clientID
+        case vaultSetupToken
+        case paymentSource
+    }
+    
+    private  enum PaymentSourceKeys: String, CodingKey {
+        case card
+    }
+    
+    private enum CardKeys: String, CodingKey {
+        case number
+        case securityCode
+        case expiry
+        case name
+    }
+    
+    // MARK: - Initializer
+    
+    // TODO: - Migrate details from VaultRequest introduced in Victoria's PR
+    
+    // MARK: - Custom Encoder
+    
+    func encode(to encoder: Encoder) throws {
+        var topLevel = encoder.container(keyedBy: TopLevelKeys.self)
+        try topLevel.encode("fake-client-id", forKey: .clientID)
+        try topLevel.encode("fake-vault-setup-token", forKey: .vaultSetupToken)
+        
+        var paymentSource = topLevel.nestedContainer(keyedBy: PaymentSourceKeys.self, forKey: .paymentSource)
+        
+        var card = paymentSource.nestedContainer(keyedBy: CardKeys.self, forKey: .card)
+        try card.encode("4111 1111 1111 1111", forKey: .number)
+        try card.encode("123", forKey: .securityCode)
+        try card.encode("2027-09", forKey: .expiry)
+        try card.encode("Sammy", forKey: .name)
+    }
+}
+
+// SAMPLE
+//{
+//  "query": "query GreetingQuery ($arg1: String) { hello (name: $arg1) { value } }",
+//  "operationName": "GreetingQuery",
+//  "variables": { "arg1": "Timothy" }
+//}
