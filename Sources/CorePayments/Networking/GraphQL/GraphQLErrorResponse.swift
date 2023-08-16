4ef956b78d8340d6eff9894965817d81e3be1482
Sammy Cannillo <scannillo@paypal.com>
2023-08-16 14:44:01 -0500

    PR Feedback - use custom coding keys for GraphQLErrorResponse

--- Sources/CorePayments/Networking/GraphQL/GraphQLErrorResponse.swift
+++ Sources/CorePayments/Networking/GraphQL/GraphQLErrorResponse.swift
@@ -3,6 +3,11 @@ import Foundation
 /// Used to parse error message details out of GraphQL HTTP response body
 struct GraphQLErrorResponse: Decodable {
     
+    enum CodingKeys: String, CodingKey {
+        case error = "error"
+        case correlationID = "correlation_id"
+    }
+    
     let error: String
-    let correlationId: String?
+    let correlationID: String?
 }
