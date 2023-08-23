2c7f841bf9ffd085c263379560f7a65e859993e2
Sammy Cannillo <scannillo@paypal.com>
2023-08-23 13:46:32 -0500

    Cleanup tests for AnalyticsService_Tests

---
+++ UnitTests/PaymentsCoreTests/MockTrackingEventsAPI.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+class MockTrackingEventsAPI: TrackingEventsAPI {
+    
+    var stubHTTPResponse: HTTPResponse?
+    var stubError: Error?
+    
+    var capturedAnalyticsEventData: AnalyticsEventData?
+    
+    override func sendEvent(with analyticsEventData: AnalyticsEventData) async throws -> HTTPResponse {
+        capturedAnalyticsEventData = analyticsEventData
+        
+        if let stubError {
+            throw stubError
+        }
+        
+        if let stubHTTPResponse {
+            return stubHTTPResponse
+        }
+        
+        throw CoreSDKError(code: 0, domain: "", errorDescription: "Stubbed responses not implemented for this mock.")
+    }
+}
