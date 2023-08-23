c2d71e773a23fd98cefc25576813bb3cfce1ce3c
Sammy Cannillo <scannillo@paypal.com>
2023-08-23 10:55:59 -0500

    PR Feedback - newline each comman in queryString

--- Sources/CardPayments/APIRequests/VaultPaymentTokensAPI.swift
+++ Sources/CardPayments/APIRequests/VaultPaymentTokensAPI.swift
@@ -37,7 +37,8 @@ class VaultPaymentTokensAPI {
                     id,
                     status,
                     links {
-                        rel, href
+                        rel,
+                        href
                     }
                 }
             }
