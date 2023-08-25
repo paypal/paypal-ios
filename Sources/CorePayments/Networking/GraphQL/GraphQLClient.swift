b9e199179847f36d04b5ad23d50cf13bb7a5b81a
Victoria Park <vicpark@paypal.com>
2023-08-25 13:25:32 -0700

    delete debug print

--- Sources/CorePayments/Networking/GraphQL/GraphQLClient.swift
+++ Sources/CorePayments/Networking/GraphQL/GraphQLClient.swift
@@ -19,7 +19,6 @@ public class GraphQLClient {
             request.addValue(value, forHTTPHeaderField: key)
         }
         let (data, response) = try await urlSession.performRequest(with: request)
-        print("🌸 \(String(data: data, encoding: .utf8))")
         guard response is HTTPURLResponse else {
             return GraphQLQueryResponse(data: nil)
         }
