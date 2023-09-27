8b5c23f02444cc2b518ae8f704aefa4b2d1748e1
Victoria Park <vicpark@paypal.com>
2023-09-27 10:37:13 -0700

    hide vaulting properties

--- Sources/CardPayments/Models/CardVaultResult.swift
+++ Sources/CardPayments/Models/CardVaultResult.swift
@@ -4,6 +4,7 @@ import CorePayments
 #endif
 
 /// The result of a vault without purchase flow.
+@_documentation(visibility: private)
 public struct CardVaultResult {
     
     /// setupTokenID of token that was updated
