8b7702c71cdef997347e2ea250ff88f6bd107d57
Victoria Park <vicpark@paypal.com>
2023-10-17 08:20:12 -0700

    combine orderCompletionViews as one

--- Demo/Demo/SwiftUIComponents/CardPaymentViews/CardOrderCompletionResultView.swift
+++ Demo/Demo/SwiftUIComponents/CardPaymentViews/CardOrderCompletionResultView.swift
@@ -9,7 +9,7 @@ struct CardOrderCompletionResultView: View {
         case .idle, .loading:
             EmptyView()
         case .loaded(let authorizedOrderResponse):
-            getAuthorizeSuccessView(orderResponse: authorizedOrderResponse)
+            getOrderSuccessView(orderResponse: authorizedOrderResponse, intent: "Authorized")
         case .error(let errorMessage):
             ErrorView(errorMessage: errorMessage)
         }
@@ -18,16 +18,16 @@ struct CardOrderCompletionResultView: View {
         case .idle, .loading:
             EmptyView()
         case .loaded(let capturedOrderResponse):
-            getCaptureSuccessView(orderResponse: capturedOrderResponse)
+            getOrderSuccessView(orderResponse: capturedOrderResponse, intent: "Captured")
         case .error(let errorMessage):
             ErrorView(errorMessage: errorMessage)
         }
     }
 
-    func getAuthorizeSuccessView(orderResponse: Order) -> some View {
+    func getOrderSuccessView(orderResponse: Order, intent: String) -> some View {
         VStack(spacing: 16) {
             HStack {
-                Text("Order Authorized")
+                Text("Order \(intent)")
                     .font(.system(size: 20))
                 Spacer()
             }
@@ -53,43 +53,6 @@ struct CardOrderCompletionResultView: View {
             }
             Text("")
                 .id("bottomView")
-        }
-        .frame(maxWidth: .infinity)
-        .padding()
-        .background(
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(.gray, lineWidth: 2)
-                .padding(5)
-        )
-    }
-
-    func getCaptureSuccessView(orderResponse: Order) -> some View {
-        VStack(spacing: 16) {
-            HStack {
-                Text("Order Captured")
-                    .font(.system(size: 20))
-                Spacer()
-            }
-            LeadingText("Order ID", weight: .bold)
-            LeadingText("\(orderResponse.id)")
-            LeadingText("Status", weight: .bold)
-            LeadingText("\(orderResponse.status)")
-            if let lastDigits = orderResponse.paymentSource?.card.lastDigits {
-                LeadingText("Card Last Digits", weight: .bold)
-                LeadingText("\(lastDigits)")
-            }
-            if let brand = orderResponse.paymentSource?.card.brand {
-                LeadingText("Brand", weight: .bold)
-                LeadingText("\(brand)")
-            }
-            if let vaultID = orderResponse.paymentSource?.card.attributes?.vault.id {
-                LeadingText("Vault ID / Payment Token", weight: .bold)
-                LeadingText("\(vaultID)")
-            }
-            if let customerID = orderResponse.paymentSource?.card.attributes?.vault.customer.id {
-                LeadingText("Customer ID", weight: .bold)
-                LeadingText("\(customerID)")
-            }
         }
         .frame(maxWidth: .infinity)
         .padding()
