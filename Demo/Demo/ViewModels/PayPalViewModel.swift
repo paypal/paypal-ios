69b69b75a761f429bfa1ad2ff5776ad475c469ec
Victoria Park <vicpark@paypal.com>
2023-10-23 16:18:53 -0700

    deprecate async native checkout start func

--- Demo/Demo/ViewModels/PayPalViewModel.swift
+++ Demo/Demo/ViewModels/PayPalViewModel.swift
@@ -57,7 +57,7 @@ class PayPalViewModel: ObservableObject {
                 self.shippingPreference = shippingPreference
 
                 let request = PayPalNativeCheckoutRequest(orderID: orderID)
-                self.payPalClient?.start(request: request)
+                self.payPalClient?.startNativeCheckout(request: request)
             } catch let error {
                 publishStateToMainThread(.mainContent(title: "Error", content: "\(error.localizedDescription)", flowComplete: true))
             }
