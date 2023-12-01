ee4f5dac3d5069f4d550121df63249979ab4c5b0
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-12-01 08:53:09 -0600

    add ScrollView to PayPalWebTransactionView

--- Demo/Demo/SwiftUIComponents/PayPalWeb/PayPalWebTransactionView.swift
+++ Demo/Demo/SwiftUIComponents/PayPalWeb/PayPalWebTransactionView.swift
@@ -5,33 +5,35 @@ struct PayPalWebTransactionView: View {
     @ObservedObject var payPalWebViewModel: PayPalWebViewModel
 
     var body: some View {
-        VStack {
-            PayPalWebStatusView(status: .approved, payPalViewModel: payPalWebViewModel)
-            ZStack {
-                Button("\(payPalWebViewModel.intent.rawValue.capitalized) Order") {
-                    Task {
-                        do {
-                            try await payPalWebViewModel.completeTransaction()
-                        } catch {
-                            print("Error capturing order: \(error.localizedDescription)")
+        ScrollView {
+            VStack {
+                PayPalWebStatusView(status: .approved, payPalViewModel: payPalWebViewModel)
+                ZStack {
+                    Button("\(payPalWebViewModel.intent.rawValue.capitalized) Order") {
+                        Task {
+                            do {
+                                try await payPalWebViewModel.completeTransaction()
+                            } catch {
+                                print("Error capturing order: \(error.localizedDescription)")
+                            }
                         }
                     }
+                    .buttonStyle(RoundedBlueButtonStyle())
+                    .padding()
+                    
+                    if payPalWebViewModel.state == .loading {
+                        CircularProgressView()
+                    }
                 }
-                .buttonStyle(RoundedBlueButtonStyle())
-                .padding()
-
-                if payPalWebViewModel.state == .loading {
-                    CircularProgressView()
+                
+                if payPalWebViewModel.state == .success {
+                    PayPalWebResultView(payPalWebViewModel: payPalWebViewModel, status: .completed)
                 }
             }
-
-            if payPalWebViewModel.state == .success {
-                PayPalWebResultView(payPalWebViewModel: payPalWebViewModel, status: .completed)
+            .onAppear {
+                payPalWebViewModel.resetState()
             }
+            Spacer()
         }
-        .onAppear {
-            payPalWebViewModel.resetState()
-        }
-        Spacer()
     }
 }
