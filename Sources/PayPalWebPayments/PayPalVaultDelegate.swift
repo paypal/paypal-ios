4d2831bfd9b1a2b5b1228db276631fd947d6e5e8
Victoria Park <vicpark@paypal.com>
2023-12-07 21:57:21 -0800

    add comments on PayPalVaultDelegate

--- Sources/PayPalWebPayments/PayPalVaultDelegate.swift
+++ Sources/PayPalWebPayments/PayPalVaultDelegate.swift
@@ -3,11 +3,23 @@ import Foundation
 import CorePayments
 #endif
 
+/// PayPalVault delegate to vault results from PayPalWebCheckoutClient
 public protocol PayPalVaultDelegate: AnyObject {
 
+    /// Notify that the PayPal vault flow finished with a successful result
+    /// - Parameters:
+    ///   - client: the PayPalWebCheckoutClient associated with delegate
+    ///   - didFinishWithResult: the successful result from the flow
     func paypal(_ paypalWebClient: PayPalWebCheckoutClient, didFinishWithVaultResult vaultTokenID: String)
 
+    /// Notify that an error occurred in the PayPal vault flow
+    /// - Parameters:
+    ///   - client: the PayPalWebCheckoutClien associated with delegate
+    ///   - didFinishWithError: the error returned by the PayPal vault flow
     func paypal(_ paypalWebClient: PayPalWebCheckoutClient, didFinishWithVaultError vaultError: CoreSDKError)
     
+    /// Notify that a cancellation occurred in the PayPal vault flow
+    /// - Parameters:
+    ///   - client: the PayPalWebCheckoutClien associated with delegate
     func paypalDidCancel(_ paypalWebClient: PayPalWebCheckoutClient)
 }
