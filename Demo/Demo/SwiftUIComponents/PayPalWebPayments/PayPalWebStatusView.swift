8ab4ab71fb511a821790d6c6c83aed81507fc8a6
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-12-11 19:15:59 -0600

    simplify PayPalWebStatusView into single view

--- Demo/Demo/SwiftUIComponents/PayPalWebPayments/PayPalWebStatusView.swift
+++ Demo/Demo/SwiftUIComponents/PayPalWebPayments/PayPalWebStatusView.swift
@@ -7,62 +7,37 @@ struct PayPalWebStatusView: View {
 
     var body: some View {
         VStack(spacing: 16) {
-            switch status {
-            case .created:
-                HStack {
-                    Text("Order Created")
-                        .font(.system(size: 20))
-                    Spacer()
-                }
-                if let order = payPalWebViewModel.createOrderResult {
-                    LeadingText("Order ID", weight: .bold)
-                    LeadingText("\(order.id)")
-                    LeadingText("Status", weight: .bold)
-                    LeadingText("\(order.status)")
-                }
-            case .approved:
-                HStack {
-                    Text("Order Approved")
-                        .font(.system(size: 20))
-                    Spacer()
-                }
-                if let order = payPalWebViewModel.createOrderResult {
-                    LeadingText("Intent", weight: .bold)
-                    LeadingText("\(payPalWebViewModel.intent)")
-                    LeadingText("Order ID", weight: .bold)
-                    LeadingText("\(order.id)")
-                    LeadingText("Payer ID", weight: .bold)
-                    LeadingText("\(payPalWebViewModel.checkoutResult?.payerID ?? "")")
-                }
-            case .completed:
-                if let order = payPalWebViewModel.transactionResult {
-                    HStack {
-                        Text("Order \(payPalWebViewModel.intent.rawValue.capitalized)d")
-                            .font(.system(size: 20))
-                        Spacer()
-                    }
-                    LeadingText("Order ID", weight: .bold)
-                    LeadingText("\(order.id)")
-                    LeadingText("Status", weight: .bold)
-                    LeadingText("\(order.status)")
+            HStack {
+                Text("Order Details")
+                    .font(.system(size: 20))
+                Spacer()
+            }
+            LeadingText("Order ID", weight: .bold)
+            LeadingText("\(payPalWebViewModel.createOrderResult?.id ?? "")")
 
-                    if let emailAddress = order.paymentSource?.paypal?.emailAddress {
-                        LeadingText("Email", weight: .bold)
-                        LeadingText("\(emailAddress)")
-                    }
+            if let status = payPalWebViewModel.order?.status {
+                LeadingText("Status", weight: .bold)
+                LeadingText("\(status)")
+            }
 
-                    if let vaultID = order.paymentSource?.paypal?.attributes?.vault.id {
-                        LeadingText("Vault ID / Payment Token", weight: .bold)
-                        LeadingText("\(vaultID)")
-                    }
+            if let payerID = payPalWebViewModel.checkoutResult?.payerID {
+                LeadingText("Payer ID", weight: .bold)
+                LeadingText("\(payerID)")
+            }
 
-                    if let customerID = order.paymentSource?.paypal?.attributes?.vault.customer.id {
-                        LeadingText("Customer ID", weight: .bold)
-                        LeadingText("\(customerID)")
-                    }
-                }
-            default:
-                Text("")
+            if let emailAddress = payPalWebViewModel.order?.paymentSource?.paypal?.emailAddress {
+                LeadingText("Email", weight: .bold)
+                LeadingText("\(emailAddress)")
+            }
+
+            if let vaultID = payPalWebViewModel.order?.paymentSource?.paypal?.attributes?.vault.id {
+                LeadingText("Vault ID / Payment Token", weight: .bold)
+                LeadingText("\(vaultID)")
+            }
+
+            if let customerID = payPalWebViewModel.order?.paymentSource?.paypal?.attributes?.vault.customer.id {
+                LeadingText("Customer ID", weight: .bold)
+                LeadingText("\(customerID)")
             }
         }
         .frame(maxWidth: .infinity)
