f1727560598da5306ca77f7c881c171d8598a187
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-12-11 19:15:34 -0600

    add picker for PayPal funding source

--- Demo/Demo/SwiftUIComponents/PayPalWebPayments/PayPalWebButtonsView.swift
+++ Demo/Demo/SwiftUIComponents/PayPalWebPayments/PayPalWebButtonsView.swift
@@ -1,25 +1,40 @@
 import SwiftUI
 import PaymentButtons
+import PayPalWebPayments
 
 struct PayPalWebButtonsView: View {
 
     @ObservedObject var payPalWebViewModel: PayPalWebViewModel
 
+    @State private var selectedFundingSource: PayPalWebCheckoutFundingSource = .paypal
+
     var body: some View {
         VStack {
             VStack(alignment: .center, spacing: 40) {
-                PayPalButton.Representable(color: .blue, size: .mini) {
-                    payPalWebViewModel.paymentButtonTapped(funding: .paypal)
+                Picker("Funding Source", selection: $selectedFundingSource) {
+                    Text("PayPal").tag(PayPalWebCheckoutFundingSource.paypal)
+                    Text("PayPal Credit").tag(PayPalWebCheckoutFundingSource.paylater)
+                    Text("Pay Later").tag(PayPalWebCheckoutFundingSource.paypalCredit)
                 }
-                .frame(maxWidth: .infinity, maxHeight: 40)
-                PayPalCreditButton.Representable(color: .black, edges: .softEdges, size: .expanded) {
-                    payPalWebViewModel.paymentButtonTapped(funding: .paypalCredit)
+                .pickerStyle(SegmentedPickerStyle())
+
+                switch selectedFundingSource {
+                case .paypalCredit:
+                    PayPalCreditButton.Representable(color: .black, size: .full) {
+                        payPalWebViewModel.paymentButtonTapped(funding: .paypalCredit)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 40)
+                case .paylater:
+                    PayPalPayLaterButton.Representable(color: .silver, edges: .softEdges, size: .full) {
+                        payPalWebViewModel.paymentButtonTapped(funding: .paylater)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 40)
+                case .paypal:
+                    PayPalButton.Representable(color: .blue, size: .full) {
+                        payPalWebViewModel.paymentButtonTapped(funding: .paypal)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 40)
                 }
-                .frame(maxWidth: .infinity, maxHeight: 40)
-                PayPalPayLaterButton.Representable(color: .silver, edges: .rounded, size: .full) {
-                    payPalWebViewModel.paymentButtonTapped(funding: .paylater)
-                }
-                .frame(maxWidth: .infinity, maxHeight: 40)
             }
             .padding(20)
             .padding()
