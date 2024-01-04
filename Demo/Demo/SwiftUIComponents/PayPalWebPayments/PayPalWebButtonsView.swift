fa1430e2eb31cfe4f9c08f5b075ab9f4c63f3bf9
stechiu <stechiu@paypal.com>
2024-01-03 17:21:10 -0800

    Fixed indentation

--- Demo/Demo/SwiftUIComponents/PayPalWebPayments/PayPalWebButtonsView.swift
+++ Demo/Demo/SwiftUIComponents/PayPalWebPayments/PayPalWebButtonsView.swift
@@ -17,43 +17,45 @@ struct PayPalWebButtonsView: View {
                 .frame(maxWidth: .infinity, maxHeight: 40)
                 PayPalCreditButton.Representable(color: .gold, edges: .softEdges, size: .expanded) {
                     payPalWebViewModel.paymentButtonTapped(funding: .paypalCredit)
-            VStack(alignment: .center, spacing: 16) {
-                HStack {
-                    Text("Checkout with PayPal")
-                        .font(.system(size: 20))
-                    Spacer()
-                }
-                .frame(maxWidth: .infinity)
-                .font(.headline)
-                Picker("Funding Source", selection: $selectedFundingSource) {
-                    Text("PayPal").tag(PayPalWebCheckoutFundingSource.paypal)
-                    Text("PayPal Credit").tag(PayPalWebCheckoutFundingSource.paylater)
-                    Text("Pay Later").tag(PayPalWebCheckoutFundingSource.paypalCredit)
-                }
-                .pickerStyle(SegmentedPickerStyle())
-
-                switch selectedFundingSource {
-                case .paypalCredit:
-                    PayPalCreditButton.Representable(color: .black, size: .full) {
-                        payPalWebViewModel.paymentButtonTapped(funding: .paypalCredit)
+                    VStack(alignment: .center, spacing: 16) {
+                        HStack {
+                            Text("Checkout with PayPal")
+                                .font(.system(size: 20))
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity)
+                        .font(.headline)
+                        Picker("Funding Source", selection: $selectedFundingSource) {
+                            Text("PayPal").tag(PayPalWebCheckoutFundingSource.paypal)
+                            Text("PayPal Credit").tag(PayPalWebCheckoutFundingSource.paylater)
+                            Text("Pay Later").tag(PayPalWebCheckoutFundingSource.paypalCredit)
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                        switch selectedFundingSource {
+                        case .paypalCredit:
+                            PayPalCreditButton.Representable(color: .gold, size: .full) {
+                                payPalWebViewModel.paymentButtonTapped(funding: .paypalCredit)
+                            }
+                        case .paylater:
+                            PayPalPayLaterButton.Representable(color: .silver, edges: .softEdges, size: .full) {
+                                payPalWebViewModel.paymentButtonTapped(funding: .paylater)
+                            }
+                        case .paypal:
+                            PayPalButton.Representable(color: .blue, size: .full) {
+                                payPalWebViewModel.paymentButtonTapped(funding: .paypal)
+                            }
+                        }
                     }
-                case .paylater:
-                    PayPalPayLaterButton.Representable(color: .silver, edges: .softEdges, size: .full) {
-                        payPalWebViewModel.paymentButtonTapped(funding: .paylater)
-                    }
-                case .paypal:
-                    PayPalButton.Representable(color: .blue, size: .full) {
-                        payPalWebViewModel.paymentButtonTapped(funding: .paypal)
-                    }
+                    .frame(height: 150)
+                    .padding(20)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.gray, lineWidth: 2)
+                            .padding(5)
+                    )
                 }
             }
-            .frame(height: 150)
-            .padding(20)
-            .background(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(.gray, lineWidth: 2)
-                    .padding(5)
-            )
         }
     }
 }
