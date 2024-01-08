c5ae8a3d29081872ebf3fbd014633107955deb8b
stechiu <stechiu@paypal.com>
2024-01-08 13:07:03 -0800

    Fix lint errors

--- Sources/PaymentButtons/PaymentButtonFont.swift
+++ Sources/PaymentButtons/PaymentButtonFont.swift
@@ -2,14 +2,14 @@ import UIKit
 
 enum PaymentButtonFont {
     /// The primary font used for non special circumstances
-    public static let paypalPrimaryFont: UIFont = UIFont(name: "PayPalOpen-Regular", size: 14) ??
+    public static let paypalPrimaryFont = UIFont(name: "PayPalOpen-Regular", size: 14) ??
         .systemFont(ofSize: UIFont.systemFontSize)
 
     /// The secondary font for sub-heading or alternative text
-    public static let paypalSecondaryFont: UIFont = UIFont(name: "PayPalOpen-Regular", size: 12) ??
+    public static let paypalSecondaryFont = UIFont(name: "PayPalOpen-Regular", size: 12) ??
         .systemFont(ofSize: UIFont.smallSystemFontSize)
 
     /// Large font of size 18
-    public static let paypalSize18Font: UIFont = UIFont(name: "PayPalOpen-Regular", size: 18) ?? 
+    public static let paypalSize18Font = UIFont(name: "PayPalOpen-Regular", size: 18) ??
         .systemFont(ofSize: UIFont.systemFontSize + 4)
 }
