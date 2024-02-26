2ee84b25187ede43795ec7ae1aa56b42b0a215bd
stechiu <stechiu@paypal.com>
2024-02-26 09:44:30 -0800

    Updated docstrings

--- Sources/PaymentButtons/PayPalCreditButton.swift
+++ Sources/PaymentButtons/PayPalCreditButton.swift
@@ -20,9 +20,9 @@ public final class PayPalCreditButton: PaymentButton {
     /// Initialize a PayPalCreditButton
     /// - Parameters:
     ///   - insets: Edge insets of the button, defining the spacing of the button's edges relative to its content.
-    ///   - color: Color of the button. Default to gold if not provided.
+    ///   - color: Color of the button. Default to `.darkBlue` if not provided.
     ///   - shape: Shape of the button. Default to `.rounded` if not provided.
-    ///   - size: Size of the button. Default to standard if not provided.
+    ///   - size: Size of the button. Default to `.standard` if not provided.
     public convenience init(
         insets: NSDirectionalEdgeInsets? = nil,
         color: Color = .darkBlue,
