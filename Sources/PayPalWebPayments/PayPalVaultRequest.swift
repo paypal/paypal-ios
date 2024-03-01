bbc4e576f8908793cfbad11761cf66a28862ca2e
Victoria Park <vicpark@paypal.com>
2024-03-01 14:45:21 -0800

    Jax feedback - docStrings for PayPalVaultRequest initializer

--- Sources/PayPalWebPayments/PayPalVaultRequest.swift
+++ Sources/PayPalWebPayments/PayPalVaultRequest.swift
@@ -9,6 +9,10 @@ public struct PayPalVaultRequest {
     /// ID for the setup token associated with the vault
     public let setupTokenID: String
 
+    /// Creates an instance of a PayPal vault request
+    /// - Parameters:
+    ///    - url: The PayPal URL for the approval web page
+    ///    - setupTokenID: An ID for the setup token to update
     public init(url: URL, setupTokenID: String) {
         self.url = url
         self.setupTokenID = setupTokenID
