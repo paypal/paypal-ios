d813b7bc5a5d896ebab1ba017bfd9b39997f7a39
sshropshire <58225613+sshropshire@users.noreply.github.com>
2024-03-19 11:18:19 -0500

    Update Environment URLs (#256)
    * Update environment URLs to use api-m.
    
    * Fix broken unit test.

--- UnitTests/PaymentsCoreTests/Environment_Tests.swift
+++ UnitTests/PaymentsCoreTests/Environment_Tests.swift
@@ -4,12 +4,12 @@ import XCTest
 class Environment_Tests: XCTestCase {
 
     func testEnvironment_sandboxURL_matchesExpectation() throws {
-        let expectedUrlString = "https://api.sandbox.paypal.com"
+        let expectedUrlString = "https://api-m.sandbox.paypal.com"
         XCTAssertEqual(Environment.sandbox.baseURL.absoluteString, expectedUrlString)
     }
 
     func testEnvironment_liveURL_matchesExpectation() throws {
-        let expectedUrlString = "https://api.paypal.com"
+        let expectedUrlString = "https://api-m.paypal.com"
         XCTAssertEqual(Environment.live.baseURL.absoluteString, expectedUrlString)
     }
 }
