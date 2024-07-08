6d2a792c4536b6faf97100227444044413381926
richherrera <ricardherrera@paypal.com>
2024-07-08 10:37:37 -0600

    Add empty VenmoPaymentView

---
+++ Demo/Demo/SwiftUIComponents/VenmoPayments/VenmoPaymentView.swift
@@ -0,0 +1,18 @@
+//
+//  VenmoPaymentView.swift
+//  Demo
+//
+//  Created by Herrera, Ricardo on 08/07/24.
+//
+
+import SwiftUI
+
+struct VenmoPaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VenmoPaymentView()
+}
