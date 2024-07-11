fc37fd69888a4f5a060b336688319a094b1571f3
richherrera <ricardherrera@paypal.com>
2024-07-11 09:51:35 -0600

    Add request properties

--- Sources/CorePayments/Eligibility/EligibilityRequest.swift
+++ Sources/CorePayments/Eligibility/EligibilityRequest.swift
@@ -1,8 +1,16 @@
-//
-//  EligibilityRequest.swift
-//  CardPayments
-//
-//  Created by Herrera, Ricardo on 11/07/24.
-//
-
 import Foundation
+
+public struct EligibilityRequest {
+
+    public let clientId: String
+    public let intent: String
+    public let currency: String
+    public let enableFunding: [String]
+    
+    public init(clientId: String, intent: String, currency: String, enableFunding: [String]) {
+        self.clientId = clientId
+        self.intent = intent
+        self.currency = currency
+        self.enableFunding = enableFunding
+    }
+}
