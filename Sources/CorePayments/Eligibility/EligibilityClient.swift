193ab5cb956361541b3e62db5e47d50b0c3162f4
richherrera <ricardherrera@paypal.com>
2024-07-11 09:51:12 -0600

    Add EligilibilityClient method

--- Sources/CorePayments/Eligibility/EligibilityClient.swift
+++ Sources/CorePayments/Eligibility/EligibilityClient.swift
@@ -1,1 +1,9 @@
+import Foundation
 
+public class EligibilityClient {
+    
+    public func check(_ eligibilityRequest: EligibilityRequest) -> EligibilityResult {
+        // TODO: - Add logic
+        .init(isVenmoEligible: false)
+    }
+}
