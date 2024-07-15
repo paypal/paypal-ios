7ff6bb1de425130387fe9dfd85318ef425a7a425
richherrera <ricardherrera@paypal.com>
2024-07-15 12:17:52 -0600

    Add parameters and initializer on Client

--- Sources/CorePayments/Eligibility/EligibilityClient.swift
+++ Sources/CorePayments/Eligibility/EligibilityClient.swift
@@ -2,8 +2,15 @@ import Foundation
 
 public final class EligibilityClient {
     
-    public func check(_ eligibilityRequest: EligibilityRequest) async throws -> EligibilityResult {
-        // TODO: - Add logic
-        .init(isVenmoEligible: false)
+    private let config: CoreConfig
+    private let eligibilityAPI: EligibilityAPI
+    
+    public init(config: CoreConfig) {
+        self.config = config
+        self.eligibilityAPI = EligibilityAPI(coreConfig: config)
+    }
+    
+    public func check(_ eligibilityRequest: EligibilityRequest = .init()) async throws -> EligibilityResult {
+        try await eligibilityAPI.check(eligibilityRequest).asResult
     }
 }
