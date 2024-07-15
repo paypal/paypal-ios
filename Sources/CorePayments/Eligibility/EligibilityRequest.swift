a5aec4c8355b67227de8bcbf5fc283e62b88d73e
richherrera <ricardherrera@paypal.com>
2024-07-15 14:44:11 -0600

    Add docstring for Request model

--- Sources/CorePayments/Eligibility/EligibilityRequest.swift
+++ Sources/CorePayments/Eligibility/EligibilityRequest.swift
@@ -1,16 +1,27 @@
 import Foundation
 
+
+/// The `EligibilityRequest` structure includes the necessary parameters to make an eligibility check request.
 public struct EligibilityRequest {
 
+    /// The currency code for the eligibility request.
+    let currency: String
+    
+    /// The intent of the eligibility request.
     let intent: EligibilityIntent
-    let currency: SupportedCurrencyType
+    
+    /// An array of supported payment methods for the request. Defaults to `[.VENMO]`.
     let enableFunding: [SupportedPaymentMethodsType]
     
     // MARK: - Initializer
     
-    public init() {
-        self.intent = .CAPTURE
-        self.currency = .USD
+    /// Creates an instance of a eligibility request
+    /// - Parameters:
+    ///    - currency: The currency code for the eligibility request.
+    ///    - intent: The intent of the eligibility request.
+    public init(currency: String, intent: EligibilityIntent) {
+        self.currency = currency
+        self.intent = intent
         self.enableFunding = [.VENMO]
     }
 }
