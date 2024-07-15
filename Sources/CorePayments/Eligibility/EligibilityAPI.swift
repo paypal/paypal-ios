1a34e59e4bccfa244283799404bf0b4bc63019b1
richherrera <ricardherrera@paypal.com>
2024-07-15 12:18:27 -0600

    Add API logic

--- Sources/CorePayments/Eligibility/EligibilityAPI.swift
+++ Sources/CorePayments/Eligibility/EligibilityAPI.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+/// API that return merchant's eligibility for different payment methods: Venmo, PayPal, PayPal Credit, Pay Later & credit card
 final class EligibilityAPI {
     
     // MARK: - Private Propertires
@@ -9,8 +10,64 @@ final class EligibilityAPI {
     
     // MARK: - Initializer
     
+    /// Initialize the eligibility API to check for payment methods eligibility
+    /// - Parameter coreConfig: configuration object
     init(coreConfig: CoreConfig) {
         self.coreConfig = coreConfig
         self.networkingClient = NetworkingClient(coreConfig: coreConfig)
     }
+    
+    /// Exposed for injecting MockNetworkingClient in tests
+    init(coreConfig: CoreConfig, networkingClient: NetworkingClient) {
+        self.coreConfig = coreConfig
+        self.networkingClient = networkingClient
+    }
+    
+    /// Checks merchants eligibility for different payment methods.
+    /// - Returns: An `EligibilityResponse` containing the result of the eligibility check.
+    func check(_ eligibilityRequest: EligibilityRequest) async throws -> EligibilityResponse {
+        let variables = EligibilityVariables(eligibilityRequest: eligibilityRequest, clientID: coreConfig.clientID)
+        let graphQLRequest = GraphQLRequest(query: Self.rawQuery, variables: variables)
+        let httpResponse = try await networkingClient.fetch(request: graphQLRequest)
+        
+        return try HTTPResponseParser().parseGraphQL(httpResponse, as: EligibilityResponse.self)
+    }
+}
+
+extension EligibilityAPI {
+    
+    static let rawQuery = """
+    query getEligibility(
+        $clientId: String!,
+        $intent: FundingEligibilityIntent!,
+        $currency: SupportedCountryCurrencies!,
+        $enableFunding: [SupportedPaymentMethodsType]
+    ){
+        fundingEligibility(
+            clientId: $clientId,
+            intent: $intent
+            currency: $currency,
+            enableFunding: $enableFunding){
+            venmo{
+                eligible
+                reasons
+            }
+            card{
+                eligible
+            }
+            paypal{
+                eligible
+                reasons
+            }
+            paylater{
+                eligible
+                reasons
+            }
+            credit{
+                eligible
+                reasons
+            }
+        }
+    }
+    """
 }
--- UnitTests/PaymentsCoreTests/Eligibility/EligibilityAPI_Tests.swift
+++ UnitTests/PaymentsCoreTests/Eligibility/EligibilityAPI_Tests.swift
@@ -1,7 +1,131 @@
 import XCTest
 @testable import CorePayments
+@testable import TestShared
 
 class EligibilityAPI_Tests: XCTestCase {
     
-    // TODO: to be implemented in a future PR
+    let mockClientID = "mockClientId"
+    let mockURLSession = MockURLSession()
+    let eligibilityRequest = EligibilityRequest()
+    
+    var sut: EligibilityAPI!
+    var coreConfig: CoreConfig!
+    var mockNetworkingClient: MockNetworkingClient!
+    
+    override func setUp() {
+        super.setUp()
+        coreConfig = CoreConfig(clientID: mockClientID, environment: .sandbox)
+        let mockHTTP = MockHTTP(coreConfig: coreConfig)
+        mockNetworkingClient = MockNetworkingClient(http: mockHTTP)
+        sut = EligibilityAPI(coreConfig: coreConfig, networkingClient: mockNetworkingClient)
+    }
+    
+    override func tearDown() {
+        coreConfig = nil
+        mockNetworkingClient = nil
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testCheckSuccess() async throws {
+        let rawQuery = """
+        query getEligibility(
+            $clientId: String!,
+            $intent: FundingEligibilityIntent!,
+            $currency: SupportedCountryCurrencies!,
+            $enableFunding: [SupportedPaymentMethodsType]
+        ){
+            fundingEligibility(
+                clientId: $clientId,
+                intent: $intent
+                currency: $currency,
+                enableFunding: $enableFunding){
+                venmo{
+                    eligible
+                    reasons
+                }
+                card{
+                    eligible
+                }
+                paypal{
+                    eligible
+                    reasons
+                }
+                paylater{
+                    eligible
+                    reasons
+                }
+                credit{
+                    eligible
+                    reasons
+                }
+            }
+        }
+        """
+        
+        _ = try? await sut.check(eligibilityRequest)
+        
+        XCTAssertEqual(mockNetworkingClient.capturedGraphQLRequest?.query, rawQuery)
+        XCTAssertNil(mockNetworkingClient.capturedGraphQLRequest?.queryNameForURL)
+        
+        let variables = mockNetworkingClient.capturedGraphQLRequest?.variables as! EligibilityVariables
+        XCTAssertEqual(variables.clientID, mockClientID)
+        XCTAssertEqual(variables.eligibilityRequest.currency.rawValue, "USD")
+        XCTAssertEqual(variables.eligibilityRequest.intent.rawValue, "CAPTURE")
+    }
+    
+    func testUpdateSetupToken_whenNetworkingClientError_bubblesError() async {
+        mockNetworkingClient.stubHTTPError = CoreSDKError(code: 123, domain: "api-client-error", errorDescription: "error-desc")
+        
+        do {
+            _ = try await sut.check(eligibilityRequest)
+            XCTFail("Expected error throw.")
+        } catch {
+            let error = error as! CoreSDKError
+            XCTAssertEqual(error.domain, "api-client-error")
+            XCTAssertEqual(error.code, 123)
+            XCTAssertEqual(error.localizedDescription, "error-desc")
+        }
+    }
+    
+    func testUpdateSetupToken_whenSuccess_returnsParsedUpdateSetupTokenResponse() async throws {
+        let successsResponseJSON = """
+        {
+            "data": {
+                "fundingEligibility": {
+                    "venmo": {
+                        "eligible": true,
+                        "reasons": []
+                    },
+                    "card": {
+                        "eligible": true
+                    },
+                    "paypal": {
+                        "eligible": true,
+                        "reasons": []
+                    },
+                    "paylater": {
+                        "eligible": true,
+                        "reasons": []
+                    },
+                    "credit": {
+                        "eligible": false,
+                        "reasons": ["INELIGIBLE DUE TO PAYLATER ELIGIBLE"]
+                    }
+                }
+            }
+        }
+        """
+        
+        let data = successsResponseJSON.data(using: .utf8)
+        let stubbedHTTPResponse = HTTPResponse(status: 200, body: data)
+        mockNetworkingClient.stubHTTPResponse = stubbedHTTPResponse
+        
+        let response = try await sut.check(eligibilityRequest)
+        XCTAssertTrue(response.isVenmoEligible)
+        XCTAssertTrue(response.isCardEligible)
+        XCTAssertTrue(response.isPayPalEligible)
+        XCTAssertTrue(response.isPayLaterEligible)
+        XCTAssertFalse(response.isCreditEligible)
+    }
 }
