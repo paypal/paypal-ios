8daa52b872bbc5756f1b3f3bc418df1a8f70c646
richherrera <ricardherrera@paypal.com>
2024-07-15 11:46:33 -0600

    Add eligibility result properties

--- Sources/CorePayments/Eligibility/EligibilityResult.swift
+++ Sources/CorePayments/Eligibility/EligibilityResult.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 public struct EligibilityResult {
-
+    
     public let isVenmoEligible: Bool
+    public let isCardEligible: Bool
+    public let isPayPalEligible: Bool
+    public let isPayLaterEligible: Bool
+    public let isCreditEligible: Bool
 }
