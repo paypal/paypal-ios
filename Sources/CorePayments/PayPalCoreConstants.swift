64093dc761a42140b8aeabd82626e66632bc1c8d
Victoria Park <vicpark@paypal.com>
2025-09-03 09:37:33 -0700

    Add UpdateClientConfig to paypal vault (#347)
    * add UpdateClientConfig to paypal vault
    
    * fix unit tests, sync w Task before URL assertion
    
    * fix lint error after merge main

--- Sources/CorePayments/PayPalCoreConstants.swift
+++ Sources/CorePayments/PayPalCoreConstants.swift
@@ -1,9 +1,11 @@
-/// This class is exposed for internal PayPal use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+/// This class is exposed for internal PayPal use only. Do not use.
+/// It is not covered by Semantic Versioning and may change or be removed at any time.
 @_documentation(visibility: private)
 public enum PayPalCoreConstants {
     
     // TODO: - Update release script to update this version #
-    /// This property is exposed for internal PayPal use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+    /// This property is exposed for internal PayPal use only. Do not use.
+    /// It is not covered by Semantic Versioning and may change or be removed at any time.
     public static let payPalSDKVersion: String = "2.0.0"
     
     public static let callbackURLScheme: String = "sdk.ios.paypal"
