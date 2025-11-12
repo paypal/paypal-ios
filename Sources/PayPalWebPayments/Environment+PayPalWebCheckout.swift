44a8f88a13341b7c7859790a6a0a9702b91462ce
sshropshire <sshropshire@paypal.com>
2025-11-12 13:41:46 -0600

    Implement custom URL for CoreConfig Environment.

--- Sources/PayPalWebPayments/Environment+PayPalWebCheckout.swift
+++ Sources/PayPalWebPayments/Environment+PayPalWebCheckout.swift
@@ -13,6 +13,8 @@ extension Environment {
             return URL(string: "https://www.sandbox.paypal.com")!
         case .live:
             return URL(string: "https://www.paypal.com")!
+        case .custom(let baseURL):
+            return URL(string: baseURL)!
         }
     }
     // swiftlint:enable force_unwrapping
