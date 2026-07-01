cfaaa7636b4e2391be05c34d33cac7f862a80d6c
NastassiaRodzik <rodzik.anastasia@gmail.com>
2026-07-01 16:38:28 -0500

    Revert UserIdentitySelection TODO, will be tacked separately

--- Demo/Demo/SwiftUIComponents/CommonComponents/UserIdentityView.swift
+++ Demo/Demo/SwiftUIComponents/CommonComponents/UserIdentityView.swift
@@ -1,26 +1,15 @@
 import SwiftUI
-import PayPalWebPayments
 
-extension PayPalUserIdentity {
-    var title: String {
-        switch self {
-        case .none:
-            "None"
-        case .emailPhone:
-            "Email/Phone"
-        case .serverSideShopperSession:
-            "SSID"
-        }
-    }
-    
-    static var all: [PayPalUserIdentity] {
-        [.none, .emailPhone(email: nil, phone: nil), .serverSideShopperSession(serverSideShopperSessionId: "")]
-    }
+// TODO: Replace with PayPalUserIdentity from SDK when feature/shopper-session-id merges
+enum UserIdentitySelection: String, CaseIterable {
+    case none = "None"
+    case buyerHints = "Email/Phone"
+    case ssid = "SSID"
 }
 
 struct UserIdentityView: View {
 
-    @Binding var selectedUserIdentity: PayPalUserIdentity
+    @Binding var selectedUserIdentity: UserIdentitySelection
     @Binding var email: String
     @Binding var phone: String
     @Binding var ssid: String
@@ -31,19 +20,19 @@ struct UserIdentityView: View {
                 .font(.subheadline)
                 .foregroundColor(.primary)
             Picker("User Identity", selection: $selectedUserIdentity) {
-                ForEach(PayPalUserIdentity.all, id: \.self) {
-                    Text($0.title).tag($0)
+                ForEach(UserIdentitySelection.allCases, id: \.self) {
+                    Text($0.rawValue).tag($0)
                 }
             }
             .pickerStyle(SegmentedPickerStyle())
             switch selectedUserIdentity {
             case .none:
                 EmptyView()
-            case .emailPhone:
+            case .buyerHints:
                 FloatingLabelTextField(placeholder: "Email (optional)", text: $email, keyboardType: .emailAddress)
                     .autocapitalization(.none)
                 FloatingLabelTextField(placeholder: "Phone (optional)", text: $phone, keyboardType: .phonePad)
-            case .serverSideShopperSession:
+            case .ssid:
                 FloatingLabelTextField(placeholder: "Server-side Shopper Session ID", text: $ssid)
             }
         }
--- Sources/PayPalWebPayments/Models/PayPalUserIdentity.swift
+++ Sources/PayPalWebPayments/Models/PayPalUserIdentity.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// Buyer identity for Shopper Session creation.
-public enum PayPalUserIdentity: Hashable {
+public enum PayPalUserIdentity {
     case serverSideShopperSession(serverSideShopperSessionId: String)
     case emailPhone(email: String?, phone: String?)
     case none
