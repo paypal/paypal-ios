f568cd949652e13a0831c10634fec47802698f19
NastassiaRodzik <rodzik.anastasia@gmail.com>
2026-07-02 16:11:51 -0500

    Strongly typed urls

--- Sources/PayPalWebPayments/Models/PayPalURLConfig.swift
+++ Sources/PayPalWebPayments/Models/PayPalURLConfig.swift
@@ -3,13 +3,13 @@ import Foundation
 /// Deep link URLs used for app switch return and cancel flows.
 public struct PayPalURLConfig {
 
-    public let returnAppURL: String
-    public let cancelAppURL: String
-    public let fallbackSchemeURL: String
+    public let returnAppURL: URL
+    public let cancelAppURL: URL
+    public let fallbackSchemeURL: URL
 
-    public init(returnAppUrl: String, cancelAppUrl: String, fallbackSchemeUrl: String) {
-        self.returnAppURL = returnAppUrl
-        self.cancelAppURL = cancelAppUrl
-        self.fallbackSchemeURL = fallbackSchemeUrl
+    public init(returnAppURL: URL, cancelAppURL: URL, fallbackSchemeURL: URL) {
+        self.returnAppURL = returnAppURL
+        self.cancelAppURL = cancelAppURL
+        self.fallbackSchemeURL = fallbackSchemeURL
     }
 }
--- UnitTests/PayPalWebPaymentsTests/PayPalURLConfig_Tests.swift
+++ UnitTests/PayPalWebPaymentsTests/PayPalURLConfig_Tests.swift
@@ -5,13 +5,13 @@ class PayPalURLConfig_Tests: XCTestCase {
 
     func testInit_setsAllUrls() {
         let config = PayPalURLConfig(
-            returnAppUrl: "myapp://return",
-            cancelAppUrl: "myapp://cancel",
-            fallbackSchemeUrl: "myapp://fallback"
+            returnAppURL: URL(string: "myapp://return")!,
+            cancelAppURL: URL(string: "myapp://cancel")!,
+            fallbackSchemeURL: URL(string: "myapp://fallback")!
         )
 
-        XCTAssertEqual(config.returnAppURL, "myapp://return")
-        XCTAssertEqual(config.cancelAppURL, "myapp://cancel")
-        XCTAssertEqual(config.fallbackSchemeURL, "myapp://fallback")
+        XCTAssertEqual(config.returnAppURL, URL(string: "myapp://return"))
+        XCTAssertEqual(config.cancelAppURL, URL(string: "myapp://cancel"))
+        XCTAssertEqual(config.fallbackSchemeURL, URL(string: "myapp://fallback"))
     }
 }
