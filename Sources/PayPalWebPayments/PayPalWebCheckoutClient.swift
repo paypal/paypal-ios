b84f822c0c3c3107b2f8e55c51bb225062b39d3b
NastassiaRodzik <rodzik.anastasia@gmail.com>
2026-07-16 16:21:41 -0500

    Fix lint

--- Sources/PayPalWebPayments/PayPalWebCheckoutClient.swift
+++ Sources/PayPalWebPayments/PayPalWebCheckoutClient.swift
@@ -934,9 +934,11 @@ public class PayPalWebCheckoutClient: NSObject {
         with error: CoreSDKError,
         completion: (Result<PayPalVaultResult, CoreSDKError>) -> Void
     ) {
-        analyticsService?.sendEvent("paypal-web-payments:vault-wo-purchase:canceled",
-                                    errorDescription: error.errorDescription,
-                                    shopperSessionId: shopperSessionID)
+        analyticsService?.sendEvent(
+            "paypal-web-payments:vault-wo-purchase:canceled",
+            errorDescription: error.errorDescription,
+            shopperSessionId: shopperSessionID
+        )
         completion(.failure(error))
     }
 }
