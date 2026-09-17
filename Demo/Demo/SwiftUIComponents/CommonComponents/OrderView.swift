1173760c7f3cba2faccb913f3dbbfd109b2f0cd5
sshropshire <sshropshire@paypal.com>
2026-09-17 11:24:22 -0500

    Add remaining CardPaymentView components before refactor.

---
+++ Demo/Demo/SwiftUIComponents/CommonComponents/OrderView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct OrderView: View {
+    
+    let order: Order
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            HStack {
+                Text("Order")
+                    .font(.system(size: 20))
+                Spacer()
+            }
+            LeadingText("Order ID", weight: .bold)
+            LeadingText("\(order.id)")
+            LeadingText("Status", weight: .bold)
+            LeadingText("\(order.status)")
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(.gray, lineWidth: 2)
+                .padding(5)
+        )
+    }
+}
