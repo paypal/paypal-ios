6ea02fb75fe97e49552e801bd602a6c222c7e744
sshropshire <sshropshire@paypal.com>
2026-09-24 14:58:38 -0400

    Add Hashable conformance to CardResult retroactively.

--- Sources/CardPayments/Models/CardResult.swift
+++ Sources/CardPayments/Models/CardResult.swift
@@ -4,7 +4,7 @@ import CorePayments
 #endif
 
 /// The result of a card payment flow.
-public struct CardResult: Hashable {
+public struct CardResult {
 
     /// The order ID associated with the transaction
     public let orderID: String
